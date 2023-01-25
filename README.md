6e62134aa28ca261257d13435ad3666055405358
root <root@fv-az190-596.22yfuiovmbsufjzmhpaxkig25b.bx.internal.cloudapp.net>
2023-01-25 13:16:04 +0000

    Add extras for lancelot-user 12 SP1A.210812.016 V14.0.0.1.SJCCNXM release-keys
    Signed-off-by: root <root@fv-az190-596.22yfuiovmbsufjzmhpaxkig25b.bx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## lancelot-user 12 SP1A.210812.016 V14.0.0.1.SJCCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6768
+- Codename: galahad
+- Brand: Redmi
+- Flavor: lancelot-user
+- Release Version: 12
+- Id: SP1A.210812.016
+- Incremental: V14.0.0.1.SJCCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Redmi/lancelot/lancelot:12/SP1A.210812.016/V14.0.0.1.SJCCNXM:user/release-keys
+- OTA version: 
+- Branch: lancelot-user-12-SP1A.210812.016-V14.0.0.1.SJCCNXM-release-keys
+- Repo: redmi_galahad_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cat product/app/MIUIHybridPlatform/MIUIHybridPlatform.apk.* 2>/dev/null >> product/app/MIUIHybridPlatform/MIUIHybridPlatform.apk
+rm -f product/app/MIUIHybridPlatform/MIUIHybridPlatform.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat system/system/app/SogouInput/SogouInput.apk.* 2>/dev/null >> system/system/app/SogouInput/SogouInput.apk
+rm -f system/system/app/SogouInput/SogouInput.apk.* 2>/dev/null
+cat system/system/app/OtaProvision/OtaProvision.apk.* 2>/dev/null >> system/system/app/OtaProvision/OtaProvision.apk
+rm -f system/system/app/OtaProvision/OtaProvision.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system/system/priv-app/MIUIGallery/MIUIGallery.apk.* 2>/dev/null >> system/system/priv-app/MIUIGallery/MIUIGallery.apk
+rm -f system/system/priv-app/MIUIGallery/MIUIGallery.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/priv-app/MiBrowser/MiBrowser.apk.* 2>/dev/null >> system/system/priv-app/MiBrowser/MiBrowser.apk
+rm -f system/system/priv-app/MiBrowser/MiBrowser.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIVideo/MIUIVideo.apk.* 2>/dev/null >> system/system/priv-app/MIUIVideo/MIUIVideo.apk
+rm -f system/system/priv-app/MIUIVideo/MIUIVideo.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIMusic/MIUIMusic.apk.* 2>/dev/null >> system/system/priv-app/MIUIMusic/MIUIMusic.apk
+rm -f system/system/priv-app/MIUIMusic/MIUIMusic.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> system/system/data-app/SmartHome/SmartHome.apk
+rm -f system/system/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/data-app/com.ss.android.ugc.aweme_15/com.ss.android.ugc.aweme_15.apk.* 2>/dev/null >> system/system/data-app/com.ss.android.ugc.aweme_15/com.ss.android.ugc.aweme_15.apk
+rm -f system/system/data-app/com.ss.android.ugc.aweme_15/com.ss.android.ugc.aweme_15.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
